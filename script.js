666fb5622da3421b554ea99b05ffc4391844b77f
romasencoroma <83155097+romasencoroma@users.noreply.github.com>
2024-11-28 14:24:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 /* This pice of code tooked from w3school*/ 
-function openNav() {
-  var w = window.innerWidth;
-  if (w > 1000){
+function openNav() {/* function absolutly remaded by Roman */
+  var w = window.innerWidth; /* we checking screen size */
+  if (w > 1000){ /* if screen bigger than 1000px sidenav will be 250px */
     document.getElementById("mySidenav").style.width = "250px";
-  } else if (w < 1000){
+  } else if (w < 1000){ /* if screen smaller than 1000px sidenav will be 150px */
     document.getElementById("mySidenav").style.width = "150px";
   }
-  if (w > 1000){
+  if (w > 1000){/* if screen bigger than 1000px sidenav will shift main div with all content */
     document.getElementById("main").style.marginLeft = "241px";
-  } else if (w < 1000){
+  } else if (w < 1000){ /* if screen bigger than 1000px sidenav will not shift main div with all content */
     document.getElementById("main").style.marginLeft = "0px";
   }
-  
-    
-    document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.4)"; /* making shadow on main div and content inside */
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -24,14 +22,13 @@ function openNav() {
     
   }
   /* End of code from w3school */
-  var w = window.innerWidth;
-  function darkM() {
+  function darkM() { /* function dark mode  */
     var element = document.getElementById("main");
-    element.classList.toggle("dark-mode");
-    document.getElementById("mainBar").classList.toggle("mainBarN")
-    document.getElementById("nature").classList.toggle("natureN")
-    document.getElementById("mySidenav").classList.toggle("sidenavN")
-    document.getElementById("nM").classList.toggle("nMN")
-    document.getElementById("cE").classList.toggle("cEN")
-    document.getElementById("cEI").classList.toggle("cEImgN")
+    element.classList.toggle("dark-mode"); /* give main div another style  */
+    document.getElementById("mainBar").classList.toggle("mainBarN") /* give top nav bar another style  */
+    document.getElementById("nature").classList.toggle("natureN") /* give another style to word nature */
+    document.getElementById("mySidenav").classList.toggle("sidenavN") /* give another style for sidenav */
+    document.getElementById("nM").classList.toggle("nMN") /* give another style for button dark mode */
+    document.getElementById("cE").classList.toggle("cEN") /* give another style for text div */
+    document.getElementById("cEI").classList.toggle("cEImgN") /* give another style for image div */
   }
