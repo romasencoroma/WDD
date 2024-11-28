27703edda9e727fbe9894dd421dd4c7842f9f245
romasencoroma <83155097+romasencoroma@users.noreply.github.com>
2024-11-28 10:27:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 /* This pice of code tooked from w3school*/ 
 function openNav() {
+  var w = window.innerWidth;
+  if (w > 1000){
     document.getElementById("mySidenav").style.width = "250px";
+  } else if (w < 1000){
+    document.getElementById("mySidenav").style.width = "150px";
+  }
+  if (w > 1000){
     document.getElementById("main").style.marginLeft = "241px";
+  } else if (w < 1000){
+    document.getElementById("main").style.marginLeft = "0px";
+  }
+  
+    
     document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
@@ -13,6 +24,7 @@ function openNav() {
     
   }
   /* End of code from w3school */
+  var w = window.innerWidth;
   function darkM() {
     var element = document.getElementById("main");
     element.classList.toggle("dark-mode");
